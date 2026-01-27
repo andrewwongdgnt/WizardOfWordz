36cdab42a515284c7778c34bddcb581c2365516e
Andrew Wong <andrew.wong.dgnt@gmail.com>
2026-01-26 22:51:01 -0800

    Fix CalculateFightEndStateUsecaseTest

--- Assets/Scenes/Game/Code/Tests/Domain/Usecase/CalculateFightEndStateUsecaseTest.cs
+++ Assets/Scenes/Game/Code/Tests/Domain/Usecase/CalculateFightEndStateUsecaseTest.cs
@@ -6,134 +6,96 @@ using static Enemy;
 
 public class CalculateFightEndStateUsecaseTest
 {
-    //private CalculateFightEndStateUsecase sut;
-    //private PlayerManager mockPlayerManager;
+    private CalculateFightEndStateUsecase sut;
+    private IPlayerManager mockPlayerManager;
 
-    //public static IEnumerable<TestCaseData> InvokeTestCases
-    //{
-    //    get
-    //    {
-    //        yield return new TestCaseData(
-    //            new List<Enemy>
-    //            {
-    //                MockEnemyUtil.GenerateEnemy(e =>
-    //                {
-    //                    e.IsDead().Returns(false);
-    //                }),  
-    //                MockEnemyUtil.GenerateEnemy(e =>
-    //                {
-    //                    e.IsDead().Returns(false);
-    //                })
-    //            },
-    //            false,
-    //            FightEndStateEnum.Ongoing
-    //        ).SetName("2 alive enemies, player is alive");
+    public static IEnumerable<TestCaseData> InvokeTestCases
+    {
+        get
+        {
+            yield return new TestCaseData(
+                new List<Enemy>
+                {
+                    MockEnemyUtil.GenerateEnemy(),
+                    MockEnemyUtil.GenerateEnemy()
+                },
+                false,
+                FightEndStateEnum.Ongoing
+            ).SetName("2 alive enemies, player is alive");
 
-    //        yield return new TestCaseData(
-    //            new List<Enemy>
-    //            {
-    //                MockEnemyUtil.GenerateEnemy(e =>
-    //                {
-    //                    e.IsDead().Returns(false);
-    //                }),
-    //                MockEnemyUtil.GenerateEnemy(e =>
-    //                {
-    //                    e.IsDead().Returns(true);
-    //                })
-    //            },
-    //            false,
-    //            FightEndStateEnum.Ongoing
-    //        ).SetName("1 dead and 1 alive enemy, player is alive");
+            yield return new TestCaseData(
+                new List<Enemy>
+                {
+                    MockEnemyUtil.GenerateEnemy(startingHealth: 0),
+                    MockEnemyUtil.GenerateEnemy()
+                },
+                false,
+                FightEndStateEnum.Ongoing
+            ).SetName("1 dead and 1 alive enemy, player is alive");
 
-    //        yield return new TestCaseData(
-    //            new List<Enemy>
-    //            {
-    //                MockEnemyUtil.GenerateEnemy(e =>
-    //                {
-    //                    e.IsDead().Returns(true);
-    //                }),
-    //                MockEnemyUtil.GenerateEnemy(e =>
-    //                {
-    //                    e.IsDead().Returns(true);
-    //                })
-    //            },
-    //            false,
-    //            FightEndStateEnum.Win
-    //        ).SetName("2 dead enemies, player is alive");
+            yield return new TestCaseData(
+                new List<Enemy>
+                {
+                    MockEnemyUtil.GenerateEnemy(startingHealth: 0),
+                    MockEnemyUtil.GenerateEnemy(startingHealth: 0)
+                },
+                false,
+                FightEndStateEnum.Win
+            ).SetName("2 dead enemies, player is alive");
 
-    //        yield return new TestCaseData(
-    //            new List<Enemy>
-    //            {
-    //                MockEnemyUtil.GenerateEnemy(e =>
-    //                {
-    //                    e.IsDead().Returns(false);
-    //                }),
-    //                MockEnemyUtil.GenerateEnemy(e =>
-    //                {
-    //                    e.IsDead().Returns(false);
-    //                })
-    //            },
-    //            true,
-    //            FightEndStateEnum.Lose
-    //        ).SetName("2 alive enemies, player is dead");
+            yield return new TestCaseData(
+                new List<Enemy>
+                {
+                    MockEnemyUtil.GenerateEnemy(),
+                    MockEnemyUtil.GenerateEnemy()
+                },
+                true,
+                FightEndStateEnum.Lose
+            ).SetName("2 alive enemies, player is dead");
 
-    //        yield return new TestCaseData(
-    //            new List<Enemy>
-    //            {
-    //                MockEnemyUtil.GenerateEnemy(e =>
-    //                {
-    //                    e.IsDead().Returns(false);
-    //                }),
-    //                MockEnemyUtil.GenerateEnemy(e =>
-    //                {
-    //                    e.IsDead().Returns(true);
-    //                })
-    //            },
-    //            true,
-    //            FightEndStateEnum.Lose
-    //        ).SetName("1 dead and 1 alive enemy, player is dead");
+            yield return new TestCaseData(
+                new List<Enemy>
+                {
+                    MockEnemyUtil.GenerateEnemy(startingHealth: 0),
+                    MockEnemyUtil.GenerateEnemy()
+                },
+                true,
+                FightEndStateEnum.Lose
+            ).SetName("1 dead and 1 alive enemy, player is dead");
 
-    //        yield return new TestCaseData(
-    //            new List<Enemy>
-    //            {
-    //                MockEnemyUtil.GenerateEnemy(e =>
-    //                {
-    //                    e.IsDead().Returns(true);
-    //                }),
-    //                MockEnemyUtil.GenerateEnemy(e =>
-    //                {
-    //                    e.IsDead().Returns(true);
-    //                })
-    //            },
-    //            true,
-    //            FightEndStateEnum.Lose
-    //        ).SetName("2 dead enemies, player is dead");
-    //    }
-    //}
+            yield return new TestCaseData(
+                new List<Enemy>
+                {
+                    MockEnemyUtil.GenerateEnemy(startingHealth: 0),
+                    MockEnemyUtil.GenerateEnemy(startingHealth: 0)
+                },
+                true,
+                FightEndStateEnum.Lose
+            ).SetName("2 dead enemies, player is dead");
+        }
+    }
 
-    //[SetUp]
-    //public void SetUp()
-    //{
-    //    mockPlayerManager = PlayerManagerTest.GenerateMock();
-    //    sut = new(playerManager: mockPlayerManager);
-    //}
+    [SetUp]
+    public void SetUp()
+    {
+        mockPlayerManager = Substitute.For<IPlayerManager>();
+        sut = new(playerManager: mockPlayerManager);
+    }
 
-    //[Test]
-    //[TestCaseSource(nameof(InvokeTestCases))]
-    //public void TestInvoke(
-    //    List<Enemy> mockEnemies,
-    //    bool isPlayerDead,
-    //    FightEndStateEnum expected
-    //    )
-    //{
-    //    mockPlayerManager.IsDead().Returns(isPlayerDead);
+    [Test]
+    [TestCaseSource(nameof(InvokeTestCases))]
+    public void TestInvoke(
+        List<Enemy> enemies,
+        bool isPlayerDead,
+        FightEndStateEnum expected
+        )
+    {
+        mockPlayerManager.IsDead().Returns(isPlayerDead);
 
-    //    FightEndStateEnum result = sut.Invoke(
-    //        enemies: mockEnemies
-    //        );
+        FightEndStateEnum result = sut.Invoke(
+            enemies: enemies
+            );
 
-    //    Assert.AreEqual(expected, result);
-
-    //    TestUtils.ClearReceivedCalls(mockEnemies);
-    //}
+        Assert.AreEqual(expected, result);
+    }
 }
